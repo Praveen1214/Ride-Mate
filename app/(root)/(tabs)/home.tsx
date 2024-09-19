1235fe750c59731690096abcc2ff30f1a1a417de
Dileepa Praveen <118685287+Praveen1214@users.noreply.github.com>
2024-09-19 21:38:38 +0530

    home page update

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,5 +1,8 @@
 import { Ionicons } from "@expo/vector-icons";
-import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import * as Location from "expo-location";
+import { router } from "expo-router";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -8,36 +11,80 @@ import {
   StatusBar,
   TouchableOpacity,
   ScrollView,
+  Keyboard,
 } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
+
+import GoogleTextInput from "@/components/GoogleTextInput";
+import Map from "@/components/Map";
+import { useLocationStore } from "@/store";
 
 const PRIMARY_COLOR = "#0C6C41";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
   const [activeTab, setActiveTab] = useState("find");
+  const [hasPermission, setHasPermission] = useState(false);
+
+  const {
+    userAddress,
+    destinationAddress,
+    setDestinationLocation,
+    setUserLocation,
+  } = useLocationStore();
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setHasPermission(false);
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      const address = await Location.reverseGeocodeAsync({
+        latitude: location.coords?.latitude!,
+        longitude: location.coords?.longitude!,
+      });
+
+      setUserLocation({
+        latitude: location.coords?.latitude,
+        longitude: location.coords?.longitude,
+        address: `${address[0].name}, ${address[0].region}`,
+      });
+    })();
+  }, []);
+
+  const handleNavigateToFindRide = () => {
+    router.push({
+      pathname: "/(root)/find-ride",
+    });
+  };
 
   return (
     <SafeAreaView className="flex-1 bg-white">
       <StatusBar barStyle="dark-content" backgroundColor="white" />
 
       {/* Map View */}
-      <View className="flex-1 bg-gray-200">
-        <Text className="text-center pt-4">Map Placeholder</Text>
+      <View className="flex flex-row items-center bg-transparent h-full">
+        <Map />
       </View>
 
       {/* Header */}
       <View className="absolute top-12 left-0 right-0 bg-white px-4 py-2 flex-row justify-between items-center">
         <View className="flex-row items-center">
-          <View className="w-10 h-10 bg-black rounded-full items-center justify-center">
+          <View className="w-9 h-9 bg-black rounded-full items-center justify-center">
             <Text className="text-white text-lg font-bold">JD</Text>
           </View>
-          <View className="flex-col gap-0 ml-3">
-            <Text className="text-lg font-semibold">Welcome Abhishek</Text>
-            <Text className="text-sm text-gray-600">Malabe</Text>
+          <View className="flex-col ml-3">
+            <Text className="text-m font-semibold leading-6">
+              Welcome! Praveen
+            </Text>
+            <Text className="text-sm text-gray-600 leading-4">Malabe</Text>
           </View>
         </View>
         <View className="w-6 h-6 items-center justify-center">
-          <Text className="text-[#0C6C41] text-xl"></Text>
+          <Icon name="bell" size={20} color="#000" />
         </View>
       </View>
 
@@ -45,36 +92,42 @@ const HomeScreen = () => {
       <View className="absolute bottom-16 left-4 right-4 bg-white rounded-lg shadow-lg mb-10">
         <View className="flex-row mb-4">
           <TouchableOpacity
-            className={`flex-1 py-3 flex-row justify-center items-center ${activeTab === "find" ? "bg-[#0C6C41]" : "bg-gray-200"} rounded-l-md`}
+            className={`flex-1 py-3 flex-row justify-center items-center ${
+              activeTab === "find" ? "bg-[#0C6C41]" : "bg-gray-200"
+            } rounded-l-md`}
             onPress={() => setActiveTab("find")}
           >
             <Ionicons
-              name={
-                activeTab === "find" ? "location-outline" : "location-outline"
-              } // Use outline icon
+              name="location-outline"
               size={18}
               color={activeTab === "find" ? "white" : "gray"}
               style={{ marginRight: 8 }}
             />
             <Text
-              className={`text-center ${activeTab === "find" ? "text-white" : "text-gray-700"} text-[15px] font-regular`}
+              className={`text-center ${
+                activeTab === "find" ? "text-white" : "text-gray-700"
+              } text-[15px] font-regular`}
             >
               Find ride
             </Text>
           </TouchableOpacity>
 
           <TouchableOpacity
-            className={`flex-1 py-3 flex-row justify-center items-center ${activeTab === "offer" ? "bg-[#0C6C41]" : "bg-gray-200"} rounded-r-md`}
+            className={`flex-1 py-3 flex-row justify-center items-center ${
+              activeTab === "offer" ? "bg-[#0C6C41]" : "bg-gray-200"
+            } rounded-r-md`}
             onPress={() => setActiveTab("offer")}
           >
             <Ionicons
-              name={activeTab === "offer" ? "location" : "location"} // Use filled icon
+              name="location"
               size={18}
               color={activeTab === "offer" ? "white" : "gray"}
               style={{ marginRight: 8 }}
             />
             <Text
-              className={`text-center ${activeTab === "offer" ? "text-white" : "text-gray-700"} text-[15px] font-regular`}
+              className={`text-center ${
+                activeTab === "offer" ? "text-white" : "text-gray-700"
+              } text-[15px] font-regular`}
             >
               Offer ride
             </Text>
@@ -85,30 +138,23 @@ const HomeScreen = () => {
           <View className="flex-row mb-3 items-center gap-3 pl-5 border-b border-gray-100">
             <Text className="text-xs text-gray-700 w-20">PICKUP</Text>
             <TextInput
-              value="Location fetched"
-              className=" font-semibold flex-1 py-2 px-3 rounded"
+              placeholder={userAddress}
+              placeholderTextColor="gray"
+              className="text-gray-700 flex-1 py-2 px-3 rounded"
             />
           </View>
 
-          <View className="flex-row mb-3 items-center gap-3 pl-5 ">
+          <View className="flex-row mb-3 items-center gap-3 pl-5">
             <Text className="text-xs text-gray-700 mb-1 w-20">DROP</Text>
-            <TextInput
-              placeholder="Where are you Drop?"
-              placeholderTextColor="gray" // Add this to show the placeholder text clearly
-              className="text-gray-700 flex-1 py-2 px-3 rounded"
-            />
+            <TouchableOpacity
+              onPress={handleNavigateToFindRide}
+              className="flex-1"
+            >
+              <Text className="text-gray-700 py-2 px-3 rounded">
+                Where are you Drop?
+              </Text>
+            </TouchableOpacity>
           </View>
-
-          {/* <View className="flex-row mb-3 items-center gap-3 pl-5">
-            <Text className="text-xs text-gray-700 mb-1 w-20">
-              DATE AND TIME
-            </Text>
-            <TextInput
-              placeholder="Select date and time"
-              placeholderTextColor="gray" // Add this to show the placeholder text clearly
-              className="text-gray-700 flex-1 py-2 px-3 rounded"
-            />
-          </View> */}
         </ScrollView>
       </View>
     </SafeAreaView>
---
+++ app/(root)/find-ride.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+const FindRide = () => {
+  return (
+    <SafeAreaView>
+      <Text>Find-Ride</Text>
+    </SafeAreaView>
+  );
+};
+
+export default FindRide;
