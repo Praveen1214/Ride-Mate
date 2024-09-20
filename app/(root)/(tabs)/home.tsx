70d3acf381bc1bf1f3c573540ef1bf964d71d5a1
Dileepa Praveen <118685287+Praveen1214@users.noreply.github.com>
2024-09-21 01:46:31 +0530

    Update home page

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -89,7 +89,7 @@ const HomeScreen = () => {
       </View>
 
       {/* Ride Form */}
-      <View className="absolute bottom-16 left-4 right-4 bg-white rounded-lg shadow-lg mb-10">
+      <View className="absolute bottom-16 left-4 right-4 bg-white rounded-lg shadow-lg mb-10 shadow-md">
         <View className="flex-row mb-4">
           <TouchableOpacity
             className={`flex-1 py-3 flex-row justify-center items-center ${
@@ -135,22 +135,28 @@ const HomeScreen = () => {
         </View>
 
         <ScrollView>
-          <View className="flex-row mb-3 items-center gap-3 pl-5 border-b border-gray-100">
-            <Text className="text-xs text-gray-700 w-20">PICKUP</Text>
+          <View className="flex-row mb-1 items-center mt-1 pl-5">
+            <Text className="text-xs text-gray-700 w-20 font-medium text-sm">PICKUP</Text>
             <TextInput
               placeholder={userAddress}
               placeholderTextColor="gray"
-              className="text-gray-700 flex-1 py-2 px-3 rounded"
+              className="text-gray-700 flex-1  px-3 rounded"
             />
           </View>
+          <View className="absolute h-[1px] bg-gray-300 top-[48px] left-16 right-12" />
+          <View className="mr-4 items-left ml-7 h-10 ">
+            <View className="w-3 h-3 bg-gray-400 rounded-full" />
+            <View className="w-0.5 flex-1 bg-gray-300 my-1 mx-1 " />
+            <View className="w-3 h-3 bg-gray-400 rounded-full" />
+          </View>
 
-          <View className="flex-row mb-3 items-center gap-3 pl-5">
-            <Text className="text-xs text-gray-700 mb-1 w-20">DROP</Text>
+          <View className="flex-row mb-3 items-center pl-5">
+            <Text className="text-xs text-gray-700 mb-1 w-20 font-medium text-sm">DROP</Text>
             <TouchableOpacity
               onPress={handleNavigateToFindRide}
               className="flex-1"
             >
-              <Text className="text-gray-700 py-2 px-3 rounded">
+              <Text className="text-gray-400 pb-2 px-3 rounded font-bold text-base">
                 Where are you Drop?
               </Text>
             </TouchableOpacity>
--- app/(root)/find-ride.tsx
+++ app/(root)/find-ride.tsx
@@ -1,11 +1,64 @@
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-const FindRide = () => {
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
+import { ArrowLeft } from "lucide-react";
+
+const OfferRideScreen = () => {
+  const [pickup, setPickup] = useState("Location fetched");
+  const [drop, setDrop] = useState("");
+
   return (
-    <SafeAreaView>
-      <Text>Find-Ride</Text>
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="bg-[#0C6C41] py-4 px-4 flex-row items-center">
+        {/* <ArrowLeft color="#fff" size={24} /> */}
+        <Text className="text-white text-xl font-bold ml-4">Offer Ride</Text>
+      </View>
+
+      <View className="p-5 flex-1">
+        <View className="flex-row">
+          <View className="flex-1">
+            <View className="mb-0">
+              <View className="flex-row items-center gap-10">
+                <Text className="text-gray-500 text-sm mb-1 font-medium">PICKUP</Text>
+                <Text className="text-black text-base">{pickup}</Text>
+              </View>
+              <View className="absolute h-[1px] bg-gray-300 top-[40px] left-16 right-0" />
+            </View>
+            <View className="mr-4 items-left ml-2 h-10 ">
+              <View className="w-3 h-3 bg-gray-400 rounded-full" />
+              <View className="w-0.5 flex-1 bg-gray-300 my-1 mx-1 " />
+              <View className="w-3 h-3 bg-gray-400 rounded-full" />
+            </View>
+            <View>
+              <View className="flex-row items-center gap-10 ">
+                <Text className="text-gray-500 text-sm mt-[25px] font-medium">DROP</Text>
+                <TextInput
+                  className="text-gray-400 text-base"
+                  value={drop}
+                  onChangeText={setDrop}
+                  placeholder="Where are you Drop ?"
+                  placeholderTextColor="#888"
+                />
+              </View>
+            </View>
+          </View>
+        </View>
+
+        <Text className="text-gray-500 text-sm mt-6 mb-2">DATE AND TIME</Text>
+
+        <TouchableOpacity className="bg-[#0C6C41] py-4 rounded-md mt-4">
+          <Text className="text-white text-center text-base font-semibold">
+            SET LOCATION ON MAP
+          </Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
 
-export default FindRide;
+export default OfferRideScreen;
