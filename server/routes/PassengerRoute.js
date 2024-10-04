8921c21219a60a6567bf7bbef13ffb3b2bb03688
Abhishek Peiris <abhisheklpeiris@gmail.com>
2024-10-04 16:57:24 +0530

    passenger-login

--- server/routes/PassengerRoute.js
+++ server/routes/PassengerRoute.js
@@ -31,5 +31,38 @@ router.route('/register').post(async (req, res) => {
     }
 });
 
+router.route('/login').post(async (req, res) => {
+
+    const {
+        contact
+    } = req.body;
+
+    try {
+        const passenger = await Passengers.findOne({ contact : contact });
+
+        if (passenger) {
+
+            const loginPassenger = {
+
+                _id: passenger._id,
+                firstname: passenger.firstname,
+                lastname: passenger.lastname,
+                email: passenger.email,
+                gender: passenger.gender,
+                contact: passenger.contact
+
+            }
+
+            return res.status(200).json({ status: "Login Success", loginPassenger });
+        }
+        else {
+            return res.status(500).json({ status: "The contact is incorrect" });
+        }
+
+    } catch (error) {
+        return res.status(500).json({ status: "Error during login", message: error });
+    }
+});
+
 
 module.exports = router;
